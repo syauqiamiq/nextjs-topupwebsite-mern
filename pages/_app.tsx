379eb9854766fc937b706fa51a37f9ac27c40bbb
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2022-03-16 12:29:29 +0700

    feat: Convert HTML Finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ import '../styles/404-not-found.css';
 import '../styles/sidebar.css';
 import '../styles/overview.css';
 import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
